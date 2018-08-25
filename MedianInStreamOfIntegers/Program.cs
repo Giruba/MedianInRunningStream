15364beab2cdba0d17e3ba315e7b5780f7435256
gibeulah <gibeulah@microsoft.com>
2018-08-26 00:45:09 +0530

    Add project files.

---
+++ MedianInStreamOfIntegers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MedianInStreamOfIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
